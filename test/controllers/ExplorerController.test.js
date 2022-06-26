c4eda0d9715eb11ac160089d0994138a598f58b1
mariel-rs <mariel.rsal@gmail.com>
2022-06-25 22:31:33 -0500

    Add ExplorerController class - ExplorerController access Reader and ExplorerService - Add unit tests of current functionalities of ExplorerController

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,85 @@
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
+
+describe("Test suite for ExplorerController", () => {
+
+    test("1. Test getExplorersByMission", () => {
+        const explorersInJava = ExplorerController.getExplorersByMission("java");
+
+        expect(explorersInJava).toStrictEqual(
+            [
+                {
+                    "name": "Woopa6",
+                    "githubUsername": "ajolonauta6",
+                    "score": 6,
+                    "mission": "java",
+                    "stacks": [
+                        "elm"
+                    ]
+                },
+                {
+                    "name": "Woopa7",
+                    "githubUsername": "ajolonauta7",
+                    "mission": "java",
+                    "score": 7,
+                    "stacks": [
+                    ]
+                },
+                {
+                    "name": "Woopa8",
+                    "githubUsername": "ajolonauta8",
+                    "score": 8,
+                    "mission": "java",
+                    "stacks": [
+                        "elm"
+                    ]
+                },
+                {
+                    "name": "Woopa9",
+                    "githubUsername": "ajolonauta9",
+                    "score": 9,
+                    "mission": "java",
+                    "stacks": [
+                        "javascript",
+                        "elixir",
+                        "groovy",
+                        "reasonML",
+                        "elm"
+                    ]
+                },
+                {
+                    "name": "Woopa10",
+                    "githubUsername": "ajolonauta10",
+                    "score": 10,
+                    "mission": "java",
+                    "stacks": [
+                        "javascript",
+                        "elixir",
+                        "groovy",
+                        "reasonML",
+                        "elm"
+                    ]
+                },
+            ] 
+        );
+    });
+
+    test("2. Test getExplorersUsernamesByMission", () => {
+
+        const usernamesInJava = ExplorerController.getExplorersUsernamesByMission("java");
+
+        expect(usernamesInJava).toStrictEqual([
+            "ajolonauta6", 
+            "ajolonauta7", 
+            "ajolonauta8", 
+            "ajolonauta9", 
+            "ajolonauta10"
+        ]);
+    });
+
+    test("3. Test getExplorersAmountByMission", () => {
+
+        const totalUsersInJava = ExplorerController.getExplorersAmountByMission("java");
+
+        expect(totalUsersInJava).toBe(5);
+    });
+});
