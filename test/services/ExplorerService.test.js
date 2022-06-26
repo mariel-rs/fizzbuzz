cfb3985b21021b9ad69592e2eb5fbd7758037a48
mariel-rs <mariel.rsal@gmail.com>
2022-06-25 21:08:26 -0500

    Add eslint and correct code style

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,5 +1,5 @@
-const Reader = require("./../../lib/utils/Reader")
-const ExplorerService = require("./../../lib/services/ExplorerService")
+const Reader = require("./../../lib/utils/Reader");
+const ExplorerService = require("./../../lib/services/ExplorerService");
 
 // Test suite
 
@@ -7,54 +7,54 @@ describe("Tests for ExplorerService", () => {
 
     test("Filter explorers by mission", () => {
 
-        const explorers = Reader.readJsonFile("test/services/testFile.json")
+        const explorers = Reader.readJsonFile("test/services/testFile.json");
 
-        const nodeExplorers = ExplorerService.filterByMission(explorers, "node")
+        const nodeExplorers = ExplorerService.filterByMission(explorers, "node");
         
         // Only 2 explorers have the Node mission
-        expect(nodeExplorers.length).toBe(2)
+        expect(nodeExplorers.length).toBe(2);
         expect(nodeExplorers).toStrictEqual(
             [
                 {
-                  "name": "Woopa1",
-                  "githubUsername": "user1",
-                  "score": 15,
-                  "mission": "node",
-                  "stacks": [
-                    "javascript",
-                    "rust"
-                  ]
+                    "name": "Woopa1",
+                    "githubUsername": "user1",
+                    "score": 15,
+                    "mission": "node",
+                    "stacks": [
+                        "javascript",
+                        "rust"
+                    ]
                 },
                 {
-                  "name": "Woopa4",
-                  "githubUsername": "user4",
-                  "score": 9,
-                  "mission": "node",
-                  "stacks": [
-                    "c++",
-                    "c",
-                    "javascript"
-                  ]
+                    "name": "Woopa4",
+                    "githubUsername": "user4",
+                    "score": 9,
+                    "mission": "node",
+                    "stacks": [
+                        "c++",
+                        "c",
+                        "javascript"
+                    ]
                 }
             ]
-        )
-    })
+        );
+    });
 
     test("Get the number of explorers by mission", () => {
-        const explorers = Reader.readJsonFile("test/services/testFile.json")
+        const explorers = Reader.readJsonFile("test/services/testFile.json");
 
-        const nodeExplorers = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+        const nodeExplorers = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
         
         // Only 2 explorers have the Node mission
-        expect(nodeExplorers).toBe(2)
-    })
+        expect(nodeExplorers).toBe(2);
+    });
 
     test("Get the explorers GitHub usernames by mission", () => {
-        const explorers = Reader.readJsonFile("test/services/testFile.json")
+        const explorers = Reader.readJsonFile("test/services/testFile.json");
 
-        const nodeExplorersUsernames = ExplorerService.getExplorersUsernamesByMission(explorers, "ruby")
+        const nodeExplorersUsernames = ExplorerService.getExplorersUsernamesByMission(explorers, "ruby");
         
         // Only 2 explorers have the Ruby mission
-        expect(nodeExplorersUsernames).toStrictEqual(["user2", "user3"])
-    })
-})
+        expect(nodeExplorersUsernames).toStrictEqual(["user2", "user3"]);
+    });
+});
--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,28 +1,28 @@
-const Reader = require("./../../lib/utils/Reader")
-const FizzbuzzService = require("./../../lib/services/FizzbuzzService")
+const Reader = require("./../../lib/utils/Reader");
+const FizzbuzzService = require("./../../lib/services/FizzbuzzService");
 
 // Test suite
 
 describe("Test suite for FizzbuzzService", () => {
 
     test("Get the tricks depending on their score", () => {
-        const explorers = Reader.readJsonFile("test/services/testFile.json")
+        const explorers = Reader.readJsonFile("test/services/testFile.json");
         
         // Woopa1 has score of 15 (multiple of 3 and 5)
-        Woopa1Explorer = FizzbuzzService.applyValidationInExplorer(explorers[0])
-        expect(Woopa1Explorer.trick).toBe("FIZZBUZZ")
+        let Woopa1Explorer = FizzbuzzService.applyValidationInExplorer(explorers[0]);
+        expect(Woopa1Explorer.trick).toBe("FIZZBUZZ");
 
         // Woopa2 has score of 1 (not a multiple of 3 nor of 5)
-        Woopa2Explorer = FizzbuzzService.applyValidationInExplorer(explorers[1])
-        expect(Woopa2Explorer.trick).toBe(1)
+        let Woopa2Explorer = FizzbuzzService.applyValidationInExplorer(explorers[1]);
+        expect(Woopa2Explorer.trick).toBe(1);
 
         // Woopa3 has score of 5 (multiple of 5)
-        Woopa3Explorer = FizzbuzzService.applyValidationInExplorer(explorers[2])
-        expect(Woopa3Explorer.trick).toBe("BUZZ")
+        let Woopa3Explorer = FizzbuzzService.applyValidationInExplorer(explorers[2]);
+        expect(Woopa3Explorer.trick).toBe("BUZZ");
 
         // Woopa4 has score of 9 (multiple of 3)
-        Woopa4Explorer = FizzbuzzService.applyValidationInExplorer(explorers[3])
-        expect(Woopa4Explorer.trick).toBe("FIZZ")
+        let Woopa4Explorer = FizzbuzzService.applyValidationInExplorer(explorers[3]);
+        expect(Woopa4Explorer.trick).toBe("FIZZ");
 
-    })
-})
+    });
+});
